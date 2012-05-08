a3bfa7b75671cf23b15642aa4a214081ea01b93c
bjornlevi <bjornlevi@gmail.com>
2012-05-08 10:48:49 -0400

    finished following

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,21 @@
+class FriendshipsController < ApplicationController
+before_filter :signed_in_user
+
+  def create
+    @user = User.find(params[:friendship][:friend_id])
+    current_user.follow!(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+
+  def destroy
+    @user = Friendship.find(params[:id]).friend
+    current_user.unfollow!(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+end
