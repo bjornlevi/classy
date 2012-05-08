42c6704cdaf8e443efb658568c0156f4bd4715ba
bjornlevi <bjornlevi@gmail.com>
2012-05-08 13:08:31 -0400

    added post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user, :dependent => :destroy
   
+  has_many :posts
+
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120508013245) do
+ActiveRecord::Schema.define(:version => 20120508170710) do
 
   create_table "blurts", :force => true do |t|
     t.string   "content"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(:version => 20120508013245) do
   add_index "friendships", ["friend_id"], :name => "index_friendships_on_friend_id"
   add_index "friendships", ["user_id", "friend_id"], :name => "index_friendships_on_user_id_and_friend_id", :unique => true
   add_index "friendships", ["user_id"], :name => "index_friendships_on_user_id"
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
