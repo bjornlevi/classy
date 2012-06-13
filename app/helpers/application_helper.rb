87cba8a5681059cfce3d7d26892696bcf26b1e54
bjornlevi <bjornlevi@gmail.com>
2012-06-13 14:25:03 -0400

    updated conflict in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module ApplicationHelper
 	def safe_html_output(content)
 		s = sanitize content
@@ -13,20 +12,3 @@ module ApplicationHelper
 		simple_format(truncate(body.gsub(/<\/?.*?>/,  ""), :length => length)).gsub(/<\/?.*?>/,  "")
 	end
 end
-=======
-module ApplicationHelper
-
-	def safe_html_output(content)
-		s = sanitize content
-		if s == ""
-			content
-		else
-			s.html_safe
-		end
-	end
-
-	def summarize(body, length)
-		simple_format(truncate(body.gsub(/<\/?.*?>/,  ""), :length => length)).gsub(/<\/?.*?>/,  "")
-	end
-end
->>>>>>> groups
