f2ab9de3310a6d8d912839e9670669821fca9c8a
bjornlevi <bjornlevi@gmail.com>
2012-09-03 14:17:31 -0400

    search updates

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,8 @@ class SearchController < ApplicationController
 	before_filter :signed_in_user
 	
 	def index
-		@search = "WHAT?!" if signed_in?
+		#TODO: restrict to user groups
+		@response = Post.find(:all, :conditions => ['content LIKE :search OR title LIKE :search', {:search => "%#{params[:search]}%"}]).paginate(page: params[:page], per_page: 10000)
+		render template: 'feeds/feeds'
 	end
 end
